e023889f139909f93db87f80be505fbd5728a487
dododoyo <dolphinmulugeta88@gmail.com>
2023-10-07 22:23:20 +0300

    latest telegraf

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,26 +1,29 @@
 const { Telegraf } = require("telegraf");
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { message } = require("telegraf/filters");
 
-bot.start((ctx) => {
-  console.log("Received /start command");
-  try {
-    return ctx.reply("Hi");
-  } catch (e) {
-    console.error("error in start action:", e);
-    return ctx.reply("Error occured");
-  }
-});
+const bot = new Telegraf("6279553839:AAHdcfjHJ0Wv3KjFP_IEgxJSCwglqv9S3xA");
 
-// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-exports.handler = async (event) => {
-  try {
-    await bot.handleUpdate(JSON.parse(event.body));
-    return { statusCode: 200, body: "" };
-  } catch (e) {
-    console.error("error in handler:", e);
-    return {
-      statusCode: 400,
-      body: "This endpoint is meant for bot and telegram communication",
-    };
-  }
-};
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+// // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
+// exports.handler = async (event) => {
+//   try {
+//     await bot.handleUpdate(JSON.parse(event.body));
+//     return { statusCode: 200, body: "" };
+//   } catch (e) {
+//     console.error("error in handler:", e);
+//     return {
+//       statusCode: 400,
+//       body: "This endpoint is meant for bot and telegram communication",
+//     };
+//   }
+// };
