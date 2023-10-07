29e46e18577f4abedcad400725bb18b7938c9589
dododoyo <dolphinmulugeta88@gmail.com>
2023-10-07 22:59:30 +0300

    added launch again

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -8,20 +8,19 @@ bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 
-// bot.launch();
-
+bot.launch();
 
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-exports.handler = async (event) => {
-  try {
-    await bot.handleUpdate(JSON.parse(event.body));
-    return { statusCode: 200, body: "" };
-  } catch (e) {
-    console.error("error in handler:", e);
-    return {
-      statusCode: 400,
-      body: "This endpoint is meant for bot and telegram communication",
-    };
-  }
-};
+// exports.handler = async (event) => {
+//   try {
+//     await bot.handleUpdate(JSON.parse(event.body));
+//     return { statusCode: 200, body: "" };
+//   } catch (e) {
+//     console.error("error in handler:", e);
+//     return {
+//       statusCode: 400,
+//       body: "This endpoint is meant for bot and telegram communication",
+//     };
+//   }
+// };
